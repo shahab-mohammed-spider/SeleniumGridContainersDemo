ca59d1321eedce63a6aba6f6e831824b058f6fbc
mohammed-shahab-spider <smohammed@spiderlogic.com>
2018-04-03 15:22:18 +0530

    Selenium Demo Code to run in Docker Containers

---
+++ SeleniumParallelTest/Hooks.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Reflection;
+
+namespace SeleniumParallelTest
+{
+    //Enum for browserType
+    public enum BrowerType
+    {
+        Chrome,
+        Firefox,
+        IE
+    }
+
+
+    //Fixture for class
+    [TestFixture]
+    public class Hooks : Base
+    {
+        private BrowerType _browserType;
+
+
+        public Hooks(BrowerType browser)
+        {
+            _browserType = browser;
+        }
+
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            ChooseDriverInstance(_browserType);            
+        }
+
+        private void ChooseDriverInstance(BrowerType browserType)
+        {
+            //var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembl‌​y().Location);
+
+            if (browserType == BrowerType.Chrome) {
+                DesiredCapabilities cap = DesiredCapabilities.Chrome();
+                cap.SetCapability("version", "");
+                cap.SetCapability("platform", "LINUX");
+                Driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), cap);
+                Driver.Manage().Window.Maximize();
+      
+
+            } 
+            else if (browserType == BrowerType.Firefox)
+            {
+                DesiredCapabilities cap = DesiredCapabilities.Firefox();
+                cap.SetCapability("version", "");
+                cap.SetCapability("platform", "LINUX");
+                Driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), cap);
+                Driver.Manage().Window.Maximize();
+            }
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            System.Threading.Thread.Sleep(8000);
+            Driver.Quit();
+        }
+
+
+    }
+}
---
+++ SeleniumParallelTest/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumParallelTest
+{
+    [TestFixture]
+    [Parallelizable]
+    public class FirefoxTesting : Hooks
+    {
+        public FirefoxTesting() : base(BrowerType.Firefox)
+        {
+
+        }
+
+        [Test]
+        public void FirefoxGoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement a = Driver.FindElement(By.Name("q"));
+                a.SendKeys("SpiderLogic");
+            a.SendKeys(Keys.Enter);
+            System.Threading.Thread.Sleep(5000);
+            //Driver.FindElement(By.Name("btnK")).Click();
+           //Assert.That(Driver.PageSource.Contains("spiderlogic"), Is.EqualTo(true),
+           //                                 "The text spiderlogic doest not exist");
+
+        }
+    }
+
+
+    [TestFixture]
+    [Parallelizable]
+    public class ChromeTesting : Hooks
+    {
+        public ChromeTesting() : base(BrowerType.Chrome)
+        {
+        }
+
+        [Test]
+        public void ChromeGoogleTest()
+        {
+
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("SpiderLogic");
+            Driver.FindElement(By.Name("btnK")).Click();
+            //Assert.That(Driver.PageSource.Contains("spiderlogic"), Is.EqualTo(true),
+            //                                "The text spiderlogic doest not exist");
+
+        }
+    }
+    [TestFixture]
+    [Parallelizable]
+    public class FirefoxTesting2 : Hooks
+    {
+        public FirefoxTesting2() : base(BrowerType.Firefox)
+        {
+
+        }
+
+        [Test]
+        public void FirefoxGoogleTest2()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement a = Driver.FindElement(By.Name("q"));
+            a.SendKeys("Wipfli");
+            a.SendKeys(Keys.Enter);
+            System.Threading.Thread.Sleep(5000);
+            //Driver.FindElement(By.Name("btnK")).Click();
+            //Assert.That(Driver.PageSource.Contains("spiderlogic"), Is.EqualTo(true),
+            //                                 "The text spiderlogic doest not exist");
+
+        }
+    }
+
+
+    [TestFixture]
+    [Parallelizable]
+    public class ChromeTesting2 : Hooks
+    {
+        public ChromeTesting2() : base(BrowerType.Chrome)
+        {
+        }
+
+        [Test]
+        public void ChromeGoogleTest2()
+        {
+
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("Wipfli");
+            Driver.FindElement(By.Name("btnK")).Click();
+            //Assert.That(Driver.PageSource.Contains("spiderlogic"), Is.EqualTo(true),
+            //                                "The text spiderlogic doest not exist");
+
+        }
+    }
+}
